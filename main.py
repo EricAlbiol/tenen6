072393f21008eaab22b1e79934d404b79ecb1089
EricAlbiol <ealbiol@institutmvm.cat>
2021-12-10 11:54:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    nombres = [x for x in range(1001) if '6' in str(x)]
+    print(nombres)
+if __name__ == '__main__':
+    main()
+
